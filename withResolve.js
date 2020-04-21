b4a0c440c624de92fac0e6c1d32383adab4bd971
Phet Dev <45811671+pechqwer@users.noreply.github.com>
2020-04-21 14:13:06 +0700

    withResolve style phet

---
+++ withResolve.js
@@ -0,0 +1,27 @@
+const withResolve = async (api) => {
+  return await new Promise((resolve, _) => {
+    api()
+      .then(res => {
+        resolve([null, res.data])
+      })
+      .catch(err => {
+        resolve([err, null])
+      })
+  })
+}
+
+export default withResolve
+
+// Example
+// import axios from 'axios'
+// import withResolve from '../js/withResolve'
+
+// const link = '/api/user'
+// const getUser = async (pid = null) => {
+//   const api = () => { return axios.get(`${link}${pid !== null ? `/${pid}` : ''}`) }
+//   const [err, result] = await withResolve(api)
+//   if (err) return err.response
+//   return result
+// }
+
+// export default getUser
