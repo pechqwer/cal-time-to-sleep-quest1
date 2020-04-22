0281522d94b5fc13bc8ee05ab136e81524c435fb
Phet Dev <45811671+pechqwer@users.noreply.github.com>
2020-04-22 21:30:16 +0700

    Create sum-obeject-in-array.js

---
+++ sum-obeject-in-array.js
@@ -0,0 +1,20 @@
+const a = [{a: 1, b:1, c:99}, {a: 1, b: 99, c: 1}, {a:2 , b:0, c: 0}]
+let arr = []
+let x2 = () => { 
+  a.forEach(x => {
+    const i = arr.findIndex(s => x.a == s.a)
+    if (i !== -1) {
+      r[i] = {
+        a: arr[i].a,
+        b: arr[i].b + x.b,
+        c: arr[i].c + x.c
+      }
+    } else {
+      arr.push(x)
+    }
+  })
+}
+
+x2()
+console.log(arr)
+// [{a: 1, b: 100, c: 100}, {a: 2, b: 0, c: 0}]
